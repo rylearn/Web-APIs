07d5c1122f6f2c632f55e2e38c43ef34d50980dc
rylearn <40655878+rylearn@users.noreply.github.com>
2018-08-22 22:19:36 -0700

    Simple Flask App

---
+++ quotes.py
@@ -0,0 +1,11 @@
+
+def funny_quotes():
+    return [{
+            "author" : "Elbert",
+            "quote" : "Do not take life too seriously"
+        },
+        {
+            "author": "Twain",
+            "quote": "Your mum gae"
+        }
+    ]
